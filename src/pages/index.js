40f2330d61380c56dc3f482ea80a8f60387f7e64
ajuxx <ajusabuyu@gmail.com>
2020-07-09 11:57:20 +0300

    sets up routes with react- router- dom

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <p>Main</p>
+      </div>
+    );
+  }
+}
+export default Main;
